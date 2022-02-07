fa0c01acf2add563cdcf0c55c10f33c21c24fe25
aravindkol <97201015+aravindkol@users.noreply.github.com>
2022-02-06 22:15:44 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const res = require('express/lib/response')
+const app = express()
+const port = 8000
+
+app.get('/myname', greeting)
+
+function greeting(req,response)
+{
+    response.send("Mahesh SE")
+}
+app.listen(port, () => {
+console.log(`check your greeting`)
+})
